4791d3d6fb1732102eec42c52f2fb9923942883b
MattCameronLVR <61762139+MattCameronLVR@users.noreply.github.com>
2020-08-24 10:56:46 +1000

    Has stuff present

---
+++ BlazorLearningToDo/Server/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using BlazorLearningToDo.Shared;
+
+namespace BlazorLearningToDo.Server.Hubs
+{
+	public class ChatHub : Hub
+	{
+		private static List<ChatMessage> trackedMessages = new List<ChatMessage>();
+
+		public async Task SendMessage(string user, string message)
+		{
+			trackedMessages.Add(new ChatMessage
+			{
+				User = user,
+				Content = message
+			});
+
+			await Clients.All.SendAsync("ReceiveMessage", user, message);
+		}
+
+		public async Task FetchState()
+		{
+			await Clients.All.SendAsync("ReceiveServerState", trackedMessages.ToArray());
+		}
+	}
+}
---
+++ BlazorLearningToDo/Shared/ChatMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorLearningToDo.Shared
+{
+	public class ChatMessage
+	{
+		public string User { get; set; }
+		public string Content { get; set; }
+	}
+}
